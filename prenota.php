e8783140cfdf24cff3bbe2d299b9747df0c50a6f
Alex_ <alessandra_vetrugno@libero.it>
2021-03-06 11:49:27 +0100

    Add configuration about errors

--- prenota.php
+++ prenota.php
@@ -1,3 +1,6 @@
 <?php
 
-ini_set( " DEBUG", "1");
+//Dice a livello dello script che gli errori verranno mostrati
+// e che non verranno loggati
+ini_set('display_errors', 1);
+ini_set('log_errors', 0);
