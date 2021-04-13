4388e896bf586b165e24684e06368f9f5075b990
Alessandra Vetrugno <alessandra_vetrugno@libero.it>
2021-04-13 12:07:25 +0200

    add login

---
+++ login.php
@@ -0,0 +1,10 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use League\Plates\Engine;
+
+//Viene creato l'oggetto per la gestione dei template
+$templates = new Engine('./view','tpl');
+
+echo $templates->render('login', []);
